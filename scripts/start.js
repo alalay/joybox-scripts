63384f892d07c833fb47f70afc84da8d37aa1987
alalay <alalay888@gmail.com>
2021-03-15 15:14:13 +0800

    add start script

---
+++ scripts/start.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const configFactory = require('../config/webpack.config');
+const createDevServerConfig = require('../config/webpackDevServer.config');
+const chalk = require('chalk');
+
+// 1.设置环境变量
+process.env.NODE_ENV = 'development';
+// 2.get config
+const config = configFactory('development');
+
+const compiler = webpack(config);
+// 3.得到server config
+const serverConfig = createDevServerConfig();
+// 4.一个dev server实例
+const devServer = new WebpackDevServer(compiler, serverConfig);
+devServer.listen(3000, () => {
+  console.log(chalk.cyan('Starting the development server'));
+});
