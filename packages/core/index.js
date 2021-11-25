3c4b50b83ed3bee08ff98232933dfad0b595ab07
alalay <alalay888@gmail.com>
2021-11-25 13:18:09 +0800

    update index.js

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
-// 开启子进程，js是单线程
-// ❓为什么要开子进程执行任务
-// 💡为了并行
-const spawn = require('cross-spawn');
-// 获取命令行参数, slice(2)从第三个元素开始截取
-const args = process.argv.slice(2);
-const script = args[0]; //"build"
-spawn.sync(
-    process.execPath, // node可执行文件
-    [require.resolve('./scripts/'+script)],
-    {stdio: 'inherit'} // 父子进程共享输入输出
-);
+// argv: [node path,joybox-scripts path, build]
+const command = process.argv[2];
+const options = process.argv.slice(3);
 
+function runScripts(command, options) {
+    // TODO
+}
 
+switch (command) {
+    case 'build':
+        runScript(command, options);
+        break;
 
-console.log('done');
+    default:
+        console.log(`Command ${command} not found`);
+        break;
+}
--- packages/core/scripts/build.js
+++ packages/core/scripts/build.js
@@ -1,42 +1,19 @@
-const fs = require("fs-extra");
-const paths = require("../../../config/paths");
-const pahts = require("../../../config/paths");
-const webpack = require("webpack");
-const chalk = require("chalk");
-/**
- * 1.设置环境变量
- */
-process.env.NODE_ENV = "production";
-/**
- * 2.获取webpack配置文件
- */
+const spawn = require('cross-spawn');
+const { hereRelative, resolveBin } = require('../utils/path-resolver');
 
-debugger;
+const webpack = resolveBin('webpack');
 
-function copyPublicFolder() {
-  fs.copySync(paths.appPublic, paths.appBuild, {
-    filter: (file) => file !== paths.appHtml, // index.html不拷贝，交由webpack插件编译处理
-  });
-}
-function build(config) {
-  let compiler = webpack(config);
-  // console.log(chalk.green(config));
-  compiler.run((err, stats) => {
-    // console.log(stats);
-    // console.log(chalk.green("Compiled Successfully"));
-  });
-}
-
-const configFactory = require("../config/webpack.config");
-
-const config = configFactory("production");
-
-/**
- * 3.如果build不为空，清空build目录
- */
-fs.emptyDirSync(paths.appBuild);
-/**
- * 4. copy public目录下的静态文件到build目录
- */
-copyPublicFolder();
-build(config);
+function build(env = 'production', _, options = []) {
+  debugger;
+  return spawn.sync(
+    webpack,
+    [
+      '--config',
+      hereRelative(__dirname, '../config/webpack.config.js'),// 需要得到config相对于build命令执行目录的相对路径
+      '--progress',
+      ...options
+    ],
+    { stdio: 'inherit', env }
+  );
+};
+build();
